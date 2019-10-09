4c1b93aec6baa5a22a358d8875a785d1b33c6f0f
mishka <m_zhvania2@cu.edu.ge>
2019-10-09 14:43:01 +0400

    Create new product attribute

---
+++ app/code/Dev/ProductComments/Model/Attribute/Source/Comments.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dev\ProductComments\Model\Attribute\Source;
+
+use Magento\Eav\Model\Entity\Attribute\Source\AbstractSource;
+
+class Comments extends AbstractSource
+{
+    public function getAllOptions (){
+        if (!$this->_options) {
+            $this->_options = [
+                ['label' => ('Yes'), 'value' => 'yes'],
+                ['label' => ('No'), 'value' => 'no']
+            ];
+        }
+        return $this->_options;
+    }
+}
